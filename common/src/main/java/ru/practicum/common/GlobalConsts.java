f6ec744c8fdc699618c0932df403fa4411bae9b7
Stanislav Makarov <stanmakarov@yandex.ru>
2023-07-19 13:49:43 +0300

    FIX доработка по ревью

--- common/src/main/java/ru/practicum/common/GlobalConsts.java
+++ common/src/main/java/ru/practicum/common/GlobalConsts.java
@@ -3,6 +3,7 @@ package ru.practicum.common;
 import lombok.experimental.UtilityClass;
 
 import java.time.format.DateTimeFormatter;
+
 @UtilityClass
 public class GlobalConsts {
     private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
